79fa084022166bbc5fe75c031c61796f6834a4ad
ChristianLambert <christianlambert412@gmail.com>
2025-02-07 15:25:25 +0100

    final update lab 2

--- Lab2/CalcED.m
+++ Lab2/CalcED.m
@@ -1,7 +1,7 @@
 function Euc_Dist = CalcED(ref,est)
 
-[L1, A1, B1] = xyz2lab(ref(1,:), ref(2,:), ref(3,:));
-[L2, A2, B2] = xyz2lab(est(1,:), est(2,:), est(3,:));
+[L1, A1, B1] = xyz2lab(ref(:,1), ref(:,2), ref(:,3));
+[L2, A2, B2] = xyz2lab(est(:,1), est(:,2), est(:,3));
 
 Euc_Dist = sqrt((L1-L2).^2 + (A1-A2).^2 + (B1-B2).^2);
 
